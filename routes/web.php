23f7c13e8a651c30690122e9689767d95bca887f
Mark van Eijk <mark@vormkracht10.nl>
2025-06-04 09:50:30 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Backstage\OgImage\Laravel\Http\Controllers\OgImageController;
 use Illuminate\Support\Facades\Route;
 
-if (app()->environment('local')) {
-    Route::get('og-image/preview', [OgImageController::class, '__invoke'])->name('og-image.html');
-}
-
-Route::get('og-image', [OgImageController::class, '__invoke'])->name('og-image.file');
+OgImage::routes();
